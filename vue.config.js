852ea3c1987223765a3fa6919ac8febc8c35fa2a
lynn <1057516363@qq.com>
2021-04-07 14:32:51 +0800

    feat: 修改map上传动作

--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,12 @@
 const uploadSourceMapWebPlugin = require('./src/tool')
-
+console.log('上传地址' + process.env.VUE_APP_FILE_UPLOAD_URL)
 module.exports = {
   lintOnSave: true, // eslint 错误处理，true表示对待eslint错误为warning，warning不会导致编译失败
   configureWebpack: {
     plugins: [
       // eslint-disable-next-line new-cap
       new uploadSourceMapWebPlugin({
-        uploadUrl: 'http://owl.lynn.cool/api/log/upload-map'
+        uploadUrl: process.env.VUE_APP_FILE_UPLOAD_URL
       })
     ]
   }
